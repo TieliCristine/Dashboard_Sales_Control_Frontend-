9739b39f0ae274510cf1903a34b399c1d8bdd6a5
Tieli Cristine <tielicristinesilva@gmail.com>
2023-06-12 18:52:03 -0300

    dashboard

---
+++ src/app/user/user/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ChartType } from "chart.js";
+
+@Component({
+  selector: 'app-user-dashboard',
+  templateUrl: './user-dashboard.component.html',
+  styleUrls: ['./user-dashboard.component.scss']
+})
+export class UserDashboardComponent {
+
+}
